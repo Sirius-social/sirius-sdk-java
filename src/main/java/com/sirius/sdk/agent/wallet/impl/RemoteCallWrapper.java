f5330f1a7085a7dc4969740dc817b43bd7768a23
Mikhail <mikelytaev@gmail.com>
2021-02-14 23:44:41 +0300

    converting array[3] to Triple<> in remote calls

--- src/main/java/com/sirius/sdk/agent/wallet/impl/RemoteCallWrapper.java
+++ src/main/java/com/sirius/sdk/agent/wallet/impl/RemoteCallWrapper.java
@@ -2,63 +2,65 @@ package com.sirius.sdk.agent.wallet.impl;
 
 
 import com.google.common.reflect.TypeToken;
-import com.google.gson.JsonObject;
 import com.sirius.sdk.agent.AgentRPC;
 import com.sirius.sdk.agent.RemoteParams;
-import com.sirius.sdk.base.JsonSerializable;
-import com.sirius.sdk.errors.sirius_exceptions.*;
 import com.sirius.sdk.utils.Pair;
+import com.sirius.sdk.utils.Triple;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.lang.reflect.TypeVariable;
 import java.util.ArrayList;
 import java.util.List;
 
-public  abstract class RemoteCallWrapper<T>  implements RemoteCall<T> {
+public abstract class RemoteCallWrapper<T> implements RemoteCall<T> {
 
+    private final TypeToken<T> typeToken = new TypeToken<T>(getClass()) {
+    };
+    private final Type type = typeToken.getType();
     AgentRPC rpc;
     T myClassT;
     Class objectClass;
-  //  TypeToken<T> type = new TypeToken<T>(getClass()) {};
-  AbstractGenericType<T> abstractGenericType;
+    //  TypeToken<T> type = new TypeToken<T>(getClass()) {};
+    AbstractGenericType<T> abstractGenericType;
     AbstractDAO<T> abstractDAo;
-
-    private final TypeToken<T> typeToken = new TypeToken<T>(getClass()) { };
-    private final Type type = typeToken.getType();
-
-    public Type getType() {
-        return type;
-    }
 
     public RemoteCallWrapper(AgentRPC rpc) {
         this.rpc = rpc;
-        abstractGenericType = new AbstractGenericType<T>(){};
-        abstractDAo = new AbstractDAO<T>(){};
+        abstractGenericType = new AbstractGenericType<T>() {
+        };
+        abstractDAo = new AbstractDAO<T>() {
+        };
     }
-
 
     public RemoteCallWrapper(AgentRPC rpc, Class objectClass) {
         this.rpc = rpc;
         this.objectClass = objectClass;
     }
 
+    public Type getType() {
+        return type;
+    }
+
     public T serializeResponse(Object object) {
         if (object instanceof JSONObject) {
-            return (T) ((JSONObject) object).toString();
+            return (T) object.toString();
 
-        }else if(object instanceof JSONArray){
+        } else if (object instanceof JSONArray) {
             List<Object> objectList = new ArrayList<>();
-            for(int i=0;i<((JSONArray) object).length();i++){
+            for (int i = 0; i < ((JSONArray) object).length(); i++) {
                 objectList.add(serializeResponse(((JSONArray) object).get(i)));
             }
-            return (T)objectList;
-        }else if(object instanceof Pair){
-           Object firstObject =  serializeResponse(((Pair) object).first);
-           Object secondObject =  serializeResponse(((Pair) object).second);
-           return  (T) new Pair(firstObject,secondObject);
+            return (T) objectList;
+        } else if (object instanceof Pair) {
+            Object firstObject = serializeResponse(((Pair) object).first);
+            Object secondObject = serializeResponse(((Pair) object).second);
+            return (T) new Pair(firstObject, secondObject);
+        } else if (object instanceof Triple) {
+            Object firstObject = serializeResponse(((Triple) object).first);
+            Object secondObject = serializeResponse(((Triple) object).second);
+            Object thirdObject = serializeResponse(((Triple) object).third);
+            return (T) new Triple(firstObject, secondObject, thirdObject);
         }
         return (T) object;
     }
@@ -67,9 +69,9 @@ public  abstract class RemoteCallWrapper<T>  implements RemoteCall<T> {
     public T remoteCall(String type, RemoteParams.RemoteParamsBuilder params) {
         try {
             Object response;
-            if(params == null){
+            if (params == null) {
                 response = rpc.remoteCall(type);
-            }else{
+            } else {
                 response = rpc.remoteCall(type, params.build());
             }
             return serializeResponse(response);
@@ -80,6 +82,6 @@ public  abstract class RemoteCallWrapper<T>  implements RemoteCall<T> {
     }
 
     public T remoteCall(String type) {
-        return remoteCall(type,null);
+        return remoteCall(type, null);
     }
 }
