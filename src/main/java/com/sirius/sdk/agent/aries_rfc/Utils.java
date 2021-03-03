ef8b67614bb869f237cc31ff40ac7c29b0eb8e37
Mikhail <mikelytaev@gmail.com>
2021-03-04 00:24:25 +0300

    sign connection

--- src/main/java/com/sirius/sdk/agent/aries_rfc/Utils.java
+++ src/main/java/com/sirius/sdk/agent/aries_rfc/Utils.java
@@ -3,10 +3,12 @@ package com.sirius.sdk.agent.aries_rfc;
 import com.sirius.sdk.agent.wallet.abstract_wallet.AbstractCrypto;
 import com.sirius.sdk.utils.Pair;
 import com.sirius.sdk.utils.StringUtils;
+import org.apache.commons.lang.ArrayUtils;
 import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
+import java.lang.reflect.Array;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
@@ -21,8 +23,22 @@ public class Utils {
     }
 
     public static JSONObject sign(AbstractCrypto crypto, JSONObject value, String verkey, boolean excludeSigData) {
-        //TODO
-        return new JSONObject();
+        byte[] timestampBytes = ByteBuffer.allocate(8).putLong(System.currentTimeMillis() / 1000).array();
+        byte[] sigDataBytes = ArrayUtils.addAll(timestampBytes, value.toString().getBytes(StandardCharsets.US_ASCII));
+        String sigSata = new String(Base64.getUrlEncoder().encode((sigDataBytes)), StandardCharsets.US_ASCII);
+
+        byte[] signatureBytes = crypto.cryptoSign(verkey, sigDataBytes);
+        String signature = new String(Base64.getUrlEncoder().encode(signatureBytes), StandardCharsets.US_ASCII);
+
+        JSONObject data = (new JSONObject()).
+                put("@type", "did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/signature/1.0/ed25519Sha512_single").
+                put("signer", verkey).put("signature", signature);
+
+        if (!excludeSigData) {
+            data.put("sig_data", sigSata);
+        }
+
+        return data;
     }
 
     public static JSONObject sign(AbstractCrypto crypto, JSONObject value, String verkey) {
