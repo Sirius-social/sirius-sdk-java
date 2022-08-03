fce8bfe35edc480d898850383f4ace7be119ea37
mikelytaev <mikelytaev@gmail.com>
2022-08-03 11:21:57 +0300

    Revert "Revert "iota public diddoc""
    This reverts commit 87743707a8d51039a168d04f78dd1483dbbda42b.

--- src/main/java/examples/iota/IotaPublicDidDoc.java
+++ src/main/java/examples/iota/IotaPublicDidDoc.java
@@ -1,14 +1,11 @@
 package examples.iota;
 
 import com.danubetech.keyformats.crypto.ByteSigner;
-import com.goterl.lazycode.lazysodium.LazySodium;
 import com.goterl.lazycode.lazysodium.LazySodiumJava;
 import com.sirius.sdk.agent.connections.Endpoint;
-import com.sirius.sdk.agent.wallet.abstract_wallet.AbstractCrypto;
 import com.sirius.sdk.hub.Context;
 import com.sirius.sdk.naclJava.LibSodium;
 import com.sirius.sdk.utils.StringUtils;
-import foundation.identity.jsonld.JsonLDException;
 import foundation.identity.jsonld.JsonLDObject;
 import info.weboftrust.ldsignatures.signer.JcsEd25519Signature2020LdSigner;
 import info.weboftrust.ldsignatures.signer.LdSigner;
@@ -18,45 +15,64 @@ import org.iota.client.MessageId;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.io.IOException;
 import java.net.URI;
-import java.security.GeneralSecurityException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import org.bitcoinj.core.Base58;
-
 public class IotaPublicDidDoc {
 
     private JSONObject payload = new JSONObject();
     Context context;
-    String pubKey;
+    String publicKeyBase58;
     String tag;
 
     public IotaPublicDidDoc(Context context) {
         this.context = context;
-        pubKey = context.getCrypto().createKey();
+        publicKeyBase58 = context.getCrypto().createKey();
         LazySodiumJava s = LibSodium.getInstance().getLazySodium();
-        byte[] inputBytes = s.bytes(pubKey);
-        byte[] outputBytes = new byte[256];
-        s.cryptoGenericHash(outputBytes, 256, inputBytes, inputBytes.length, null, 0);
+        byte[] inputBytes = s.bytes(publicKeyBase58);
+        byte[] outputBytes = new byte[32];
+        s.cryptoGenericHash(outputBytes, 32, inputBytes, inputBytes.length, null, 0);
         tag = StringUtils.bytesToBase58String(outputBytes);
         payload.put("id", "did:iota:" + tag);
     }
 
     private IotaPublicDidDoc(JSONObject payload) {
         this.payload = payload;
+        tag = this.payload.optString("id").substring("did:iota:".length());
+        JSONArray authentications = this.payload.getJSONArray("authentication");
+        JSONObject authentication = (JSONObject) authentications.get(0);
+        this.publicKeyBase58 = authentication.optString("publicKeyBase58");
+    }
+
+    public JSONObject getPayload() {
+        return this.payload;
+    }
+
+    public String getDid() {
+        return this.payload.optString("id");
+    }
+
+    public String getTag() {
+        return this.tag;
     }
 
     public static IotaPublicDidDoc load(String did) {
-        MessageId[] fetchedMessageIds = node().getMessage().indexString(did);
+        String tag;
+        if (did.startsWith("did:iota:"))
+            tag = did.substring("did:iota:".length());
+        else
+            tag = did;
+        MessageId[] fetchedMessageIds = node().getMessage().indexString(tag);
         if (fetchedMessageIds.length > 0) {
             Message msg = node().getMessage().data(fetchedMessageIds[0]);
             if (msg.payload().isPresent()) {
-                JSONObject obj = new JSONObject(msg.payload().get().toString());
+                JSONObject obj = new JSONObject(new String (msg.payload().get().asIndexation().data()));
+                System.out.println(obj);
+                //TODO: check proof
                 return new IotaPublicDidDoc(obj);
             }
         }
@@ -104,14 +120,14 @@ public class IotaPublicDidDoc {
     }
 
     private void proof() {
-        ByteSigner byteSigner = new IndyWalletSigner(context.getCrypto(), pubKey);
+        ByteSigner byteSigner = new IndyWalletSigner(context.getCrypto(), publicKeyBase58);
 
         JSONArray authentication = new JSONArray();
         authentication.put(new JSONObject()
                 .put("id", this.payload.optString("id") + "#sign-0")
                 .put("controller", this.payload.optString("id"))
                 .put("type", "Ed25519VerificationKey2018")
-                .put("publicKeyBase58", this.pubKey));
+                .put("publicKeyBase58", this.publicKeyBase58));
 
         DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd'T'hh:mm:ss'Z'");
         this.payload.put("id", this.payload.optString("id"));
--- src/main/java/examples/iota/Main.java
+++ src/main/java/examples/iota/Main.java
@@ -3,6 +3,7 @@ package examples.iota;
 import com.danubetech.keyformats.crypto.ByteSigner;
 import com.nimbusds.jose.shaded.json.JSONArray;
 import com.sirius.sdk.agent.aries_rfc.feature_0160_connection_protocol.messages.Invitation;
+import com.sirius.sdk.agent.connections.Endpoint;
 import com.sirius.sdk.hub.MobileContext;
 import com.sirius.sdk.hub.MobileHub;
 import com.sirius.sdk.utils.Pair;
@@ -19,6 +20,8 @@ import java.net.URI;
 import java.security.GeneralSecurityException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 
@@ -54,40 +57,12 @@ public class Main {
 
         MobileContext context = new MobileContext(mobileConfig);
 
-        Pair<String, String> didVk = context.getDid().createAndStoreMyDid(null, null, true);
-        ByteSigner byteSigner = new IndyWalletSigner(context.getCrypto(), didVk.second);
+        IotaPublicDidDoc didDoc = new IotaPublicDidDoc(context);
+        Endpoint endpoint = new Endpoint("address", Arrays.asList("key1"));
+        didDoc.setEndpoint(endpoint);
+        didDoc.submit();
 
-        JSONArray authentication = new JSONArray();
-        authentication.add(new JSONObject()
-                .put("id", "did:iota:" + didVk.first + "#sign-0")
-                .put("controller", "did:iota:" + didVk.first)
-                .put("type", "Ed25519VerificationKey2018")
-                .put("publicKeyBase58", didVk.second));
-
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd'T'hh:mm:ss'Z'");
-        JSONObject didDoc = new JSONObject()
-                .put("id", "did:iota:" + didVk.first)
-                .put("authentication", authentication)
-                .put("created", dateFormat.format(new Date()))
-                .put("updated", dateFormat.format(new Date()));
-
-        LdSigner ldSigner = new JcsEd25519Signature2020LdSigner(byteSigner);
-        ldSigner.setVerificationMethod(URI.create("did:iota:" + didVk.first + "#sign-0"));
-
-        JsonLDObject jsonLdObject = JsonLDObject.fromJson(didDoc.toString());
-        JSONObject proof = new JSONObject(ldSigner.sign(jsonLdObject).toJson());
-
-        JSONObject signedDidDoc = new JSONObject(didDoc.toString())
-                .put("proof", proof);
-
-        System.out.println(signedDidDoc);
-
-        Client iota = node();
-        org.iota.client.Message message = iota.message().
-                withIndexString(didVk.first).
-                withDataString(signedDidDoc.toString()).
-                finish();
-        System.out.println(
-                "Message sent: https://explorer.iota.org/mainnet/message/" + message.id().toString());
+        IotaPublicDidDoc didDoc2 = IotaPublicDidDoc.load(didDoc.getDid());
+        assert didDoc.getPayload().equals(didDoc2.getPayload());
     }
 }
