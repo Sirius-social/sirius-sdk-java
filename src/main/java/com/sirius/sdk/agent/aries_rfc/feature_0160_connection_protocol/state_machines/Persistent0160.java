5234b394a67e62b89600df96e3c901442492dee5
Mikhail Lytaev <mikelytaev@gmail.com>
2022-08-09 01:31:31 +0300

    persistent 0160

--- src/main/java/com/sirius/sdk/agent/aries_rfc/feature_0160_connection_protocol/state_machines/Persistent0160.java
+++ src/main/java/com/sirius/sdk/agent/aries_rfc/feature_0160_connection_protocol/state_machines/Persistent0160.java
@@ -3,34 +3,50 @@ package com.sirius.sdk.agent.aries_rfc.feature_0160_connection_protocol.state_ma
 import com.sirius.sdk.agent.aries_rfc.DidDoc;
 import com.sirius.sdk.agent.aries_rfc.feature_0160_connection_protocol.messages.ConnRequest;
 import com.sirius.sdk.agent.aries_rfc.feature_0160_connection_protocol.messages.ConnResponse;
+import com.sirius.sdk.agent.aries_rfc.feature_0160_connection_protocol.messages.Invitation;
 import com.sirius.sdk.agent.connections.Endpoint;
 import com.sirius.sdk.agent.listener.Event;
+import com.sirius.sdk.agent.model.Entity;
 import com.sirius.sdk.agent.pairwise.Pairwise;
 import com.sirius.sdk.agent.pairwise.TheirEndpoint;
 import com.sirius.sdk.errors.sirius_exceptions.SiriusInvalidMessage;
 import com.sirius.sdk.hub.Context;
+import com.sirius.sdk.messaging.Message;
 import com.sirius.sdk.utils.Pair;
 import org.json.JSONObject;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Persistent0160 {
 
     private static Map<String, JSONObject> invKeyToPairwise = new HashMap<>();
 
-    public static boolean receive(Context context, Event event) {
+    public static Optional<Pairwise> receive(Context context, Event event) {
         if (event.message() instanceof ConnRequest) {
-            receive(context, (ConnRequest) event.message(), event.getRecipientVerkey(), context.getEndpointWithEmptyRoutingKeys());
-            return true;
+            receiveRequest(context, (ConnRequest) event.message(), event.getRecipientVerkey(), context.getEndpointWithEmptyRoutingKeys());
         } else if (event.message() instanceof ConnResponse) {
-            receive(context, (ConnResponse) event.message());
+            return Optional.ofNullable(receiveResponse(context, (ConnResponse) event.message()));
+        } else {
+            return receiveAck(context, event);
         }
-        return false;
+        return Optional.empty();
     }
 
-    private static void receive(Context context, ConnRequest request, String connectionKeyBase58, Endpoint myEndpoint) {
+    public static void acceptInvitation(Context context, Invitation invitation, String myLabel) {
+        Pair<String, String> myDidVk = context.getDid().createAndStoreMyDid();
+        ConnRequest request = ConnRequest.builder().
+                setLabel(myLabel).
+                setDid(myDidVk.first).
+                setVerkey(myDidVk.second).
+                setEndpoint(context.getEndpointAddressWithEmptyRoutingKeys()).
+                setDocUri(invitation.getDocUri()).
+                build();
+
+        String connectionKey = invitation.recipientKeys().get(0);
+        context.sendMessage(request, Arrays.asList(connectionKey), invitation.endpoint(), myDidVk.second, Arrays.asList());
+    }
+
+    private static void receiveRequest(Context context, ConnRequest request, String connectionKeyBase58, Endpoint myEndpoint) {
         ConnRequest.ExtractTheirInfoRes theirInfo;
         try {
             theirInfo = request.extractTheirInfo();
@@ -70,9 +86,40 @@ public class Persistent0160 {
                                 put("address", theirInfo.endpoint).
                                 put("routing_keys", theirInfo.routingKeys)).
                         put("did_doc", theirDidDoc)));
+
+        invKeyToPairwise.put(connectionKeyBase58, pairwise);
     }
 
-    private static void receive(Context context, ConnResponse response) {
+    private static Pairwise receiveResponse(Context context, ConnResponse response) {
 
+        return null;
+    }
+
+    private static Optional<Pairwise> receiveAck(Context context, Event event) {
+        String senderVk = event.getSenderVerkey();
+        for (Map.Entry<String, JSONObject> e : invKeyToPairwise.entrySet()) {
+            JSONObject o = e.getValue();
+            if (o.optJSONObject("their").optString("verkey").equals(senderVk)) {
+                Pairwise.Me me = new Pairwise.Me(
+                        o.optJSONObject("me").optString("did"),
+                        o.optJSONObject("me").optString("verkey"),
+                        o.optJSONObject("me").optJSONObject("did_doc"));
+
+                List<String> routingKeys = new ArrayList<>();
+                for (Object k : o.optJSONObject("their").optJSONArray("routing_keys"))
+                    routingKeys.add((String) k);
+                Pairwise.Their their = new Pairwise.Their(
+                        o.optJSONObject("their").optString("did"),
+                        o.optJSONObject("their").optString("label"),
+                        o.optJSONObject("their").optJSONObject("endpoint").optString("address"),
+                        o.optJSONObject("their").optString("verkey"),
+                        routingKeys
+                );
+                Pairwise pairwise = new Pairwise(me, their, o);
+                invKeyToPairwise.remove(e.getKey());
+                return Optional.of(pairwise);
+            }
+        }
+        return Optional.empty();
     }
 }
